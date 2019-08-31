5bf196a7639d46b9d1a3b4495e224dbcbcb8e06e
1511164 hcmut.edu.vn <1511164@hcmut.edu.vn>
2019-08-31 11:21:43 +0700

    Add project files.

---
+++ Helper/XAFHelper.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateTool.Helper
+{
+    public class XAFHelper
+    {
+
+        public static string[] libraries = {"DevExpress.ExpressApp.ConditionalAppearance", "DevExpress.ExpressApp.DC",
+                                     "DevExpress.ExpressApp.Model", "DevExpress.Persistent.Base",
+                                     "DevExpress.Persistent.BaseImpl", "DevExpress.Xpo", "System", "System.Collections.Generic",
+                                     "System.Linq", "System.Text", "System.Threading.Tasks"};
+        
+        /// <summary>
+        /// Create C - sharp file Business Object
+        /// Author : Duong Nguyen Tan Hoa
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="fileDir"></param>
+        /// <param name="countFrom"></param>
+        public static void XAFMakeFile(string fileName, string fileDir, out string createdFileName)
+        {
+            string filePath  = fileDir + @"\" + fileName + ".cs";
+            createdFileName = "";
+            /*file existed*/
+            if(File.Exists(filePath))
+            {
+                int count = 0;
+                do
+                {
+                    count++;
+                    filePath = fileDir + @"\" + fileName + "(" + count + ")" + ".cs";
+                }
+                while(File.Exists(filePath));
+                createdFileName = fileName + "(" + count + ")" + ".cs";
+                File.Create(filePath).Close();
+            }
+            else
+            {
+                createdFileName = fileName + ".cs";
+                File.Create(filePath).Close();
+            }
+        }
+        
+        /// <summary>
+        /// write content for C - Sharp file
+        /// </summary>
+        /// <param name="nameSpace"></param>
+        /// <param name="props"></param>
+        /// <param name="fileName"></param>
+        /// <param name="fileDir"></param>
+        public static void XAFWrite(string nameSpace, string className, string fileName, string fileDir)
+        {
+            string filePath = fileDir + @"\" + fileName;
+            
+            if(File.Exists(filePath))
+            {
+                XAFInitFile(nameSpace, filePath, className);
+                XAFInsertProperty("fullName", "string", filePath);
+                XAFInsertCollection("TrainingClass", "Employee-TrainingClass", filePath);
+
+                /*End of file*/
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine("\t}");
+                    writer.WriteLine("}");
+                    /*Close file*/
+                    writer.Close();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Init for the file
+        /// </summary>
+        /// <param name="nameSpace"></param>
+        /// <param name="fileName"></param>
+        /// <param name="fileDir"></param>
+        public static void XAFInitFile(string nameSpace, string filePath, string className)
+        {
+            if(File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    foreach(string library in libraries)
+                    {
+                        string usingLibrary = "using " + library + ";";
+                        writer.WriteLine(usingLibrary);
+                    }
+
+                    writer.WriteLine("namespace " + nameSpace);
+                    writer.WriteLine("{");
+                    writer.WriteLine("\t[Persistent(@\"" + className + "\")]");
+                    writer.WriteLine("\t[DefaultClassOptions]");
+                    writer.WriteLine("\t"+ "public class " + className + ": BaseObject");
+                    writer.WriteLine("\t{");
+                    writer.WriteLine("\t\tpublic " + className + "(Session session) : base(session) { }");
+                    writer.WriteLine("\t\tpublic override void AfterConstruction()");
+                    writer.WriteLine("\t\t{");
+                    writer.WriteLine("\t\t\tbase.AfterConstruction();");
+                    writer.WriteLine("\t\t}");
+                    /*Close file*/
+                    writer.Close();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Write contnnt for a property
+        /// </summary>
+        /// <param name="propsName"></param>
+        /// <param name="type"></param>
+        /// <param name="filePath"></param>
+        public static void XAFInsertProperty(string propsName, string type, string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine("\t\tprivate " + type + " _" + propsName + ";");
+                    writer.WriteLine("\t\tpublic " + type + " " +propsName);
+                    writer.WriteLine("\t\t{");
+                    writer.WriteLine("\t\t\tget => " + "_" + propsName + ";");
+                    writer.WriteLine("\t\t\tset => SetPropertyValue(nameof(" + propsName + "), ref " + "_" + propsName + ", value );");
+                    writer.WriteLine("\t\t}");
+                    /*Close file*/
+                    writer.Close();
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Insert for XPCollection
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="associationName"></param>
+        /// <param name="filePath"></param>
+        public static void XAFInsertCollection(string type, string associationName, string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine("\t\t[Association(\"" + associationName + "\")");
+                    writer.Write("\t\tpublic XPCollection<" + type + "> ");
+                    writer.Write(type + "es { get => GetCollection<" + type +">(");
+                    writer.WriteLine("nameof(" + type +"es)); }");
+                    /*Close file*/
+                    writer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,165 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using Microsoft.Win32;
+using GenerateTool.Helper;
+using System.ComponentModel;
+
+namespace GenerateTool.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Data Base Generate Tool";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            /*Init Command*/
+            getInputFile = new DelegateCommand(getInputFileExecute);
+            getOutFolder = new DelegateCommand(getOutFolderExecute);
+            generate = new DelegateCommand(generateExecute);
+
+            /*Init for BackgroundWorker*/
+            _generateWorker = new BackgroundWorker
+            {
+                WorkerReportsProgress = true,
+                WorkerSupportsCancellation = true
+            };
+            _generateWorker.DoWork += generateWorker;
+            _generateWorker.ProgressChanged += backgroundworker_PogressChange;
+            _generateWorker.RunWorkerCompleted += backgroundWorker_RunComplete;
+        }
+
+        private string _inputFile;
+        public string inputFile
+        {
+            get => _inputFile;
+            set
+            {
+                SetProperty(ref _inputFile, value);
+                RaisePropertyChanged(nameof(inputFile));
+            }
+        }
+
+        private string _outFolder;
+        public string outFolder
+        {
+            get => _outFolder;
+            set
+            {
+                SetProperty(ref _outFolder, value);
+                RaisePropertyChanged(nameof(outFolder));
+            }
+        }
+
+        private int _startSheet;
+        public int startSheet
+        {
+            get => _startSheet;
+            set
+            {
+                SetProperty(ref _startSheet, value);
+                RaisePropertyChanged(nameof(startSheet));
+            }
+        }
+
+        private int _endSheet;
+        public int endSheet
+        {
+            get => _endSheet;
+            set
+            {
+                SetProperty(ref _endSheet, value);
+                RaisePropertyChanged(nameof(endSheet));
+            }
+        }
+
+        private string _sysDate = DateTime.Today.ToString("dd/MM/yyyy");
+        public string sysDate
+        {
+            get => _sysDate;
+            set
+            {
+                SetProperty(ref _sysDate, value);
+            }
+        }
+
+        private int _progress;
+        public int Progress
+        {
+            get => _progress;
+            set
+            {
+                SetProperty(ref _progress, value);
+                RaisePropertyChanged(nameof(Progress));
+            }
+        }
+
+        public BackgroundWorker _generateWorker;
+        
+        public DelegateCommand getInputFile { get; set; }
+        public void getInputFileExecute()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog().Equals(true))
+            {
+                inputFile = dialog.FileName;
+            }
+        }
+
+        public DelegateCommand getOutFolder { get; set; }
+        public void getOutFolderExecute()
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    outFolder = dialog.SelectedPath;
+                }
+            }
+        }
+        
+        public DelegateCommand generate { get; set; }
+        public void generateExecute()
+        {
+            if(_generateWorker.IsBusy != true)
+            {
+                _generateWorker.RunWorkerAsync();
+            }
+        }
+        
+        /*Background worker*/
+        private void generateWorker(object sender, DoWorkEventArgs e)
+        {
+            var worker = sender as BackgroundWorker;
+            
+            if(worker.CancellationPending)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    XAFHelper.XAFMakeFile("User", outFolder, out string createdFileName);
+                    XAFHelper.XAFWrite("HRM.Module.BusinessObject", "User", createdFileName, outFolder);
+                    worker.ReportProgress(i * 10 + 10);
+                }
+            }
+        }
+        
+        private void backgroundworker_PogressChange(object sender, ProgressChangedEventArgs e)
+        {
+            Progress = e.ProgressPercentage;
+        }
+        
+        private void backgroundWorker_RunComplete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Generate complete");
+        }
+    }
+}
